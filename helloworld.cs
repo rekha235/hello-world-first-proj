fa7c2540e813b4a66b94c15e6fc767ac68d4c7c1
rekha235 <surekha_potta@yahoo.com>
2016-12-04 11:09:36 +0530

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Class1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
